7fbcefa905038f70f4f618fa4adb54d76db851d4
Geo Feid <fidakisgeorge@yahoo.com>
2021-12-09 01:13:26 +0200

    Double click test

--- CrowdFundingMVC/wwwroot/js/site.js
+++ CrowdFundingMVC/wwwroot/js/site.js
@@ -4,12 +4,50 @@
 // Write your JavaScript code.
 // Initialize tooltips
 
-document.addEventListener("click", function (e) {
+/*document.addEventListener("click", function (e) {
     if (e.target.classList.contains("gallery-item")) {
         const src = e.target.getAttribute("src");
         document.querySelector(".modal-img").src = src;
         const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
         myModal.show();
     }
-})
+})*/
+
+
+/* var form = document.getElementById(clicked_id);
+var submitButton = document.getElementById(clicked_id);
+
+
+form.addEventListener('submit', function () {
+
+    // Disable the submit button
+    submitButton.setAttribute('disabled', 'disabled');
+
+    // Change the "Submit" text
+    submitButton.value = 'Please wait...';
+
+}, false);*/
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+
+
+var tryNumber = 0;
+jQuery('input[type=submit]').click(function (event) {
+    var self = $(this);
+
+    if (self.closest('form').valid()) {
+        if (tryNumber > 0) {
+            tryNumber++;
+            alert('Your form has been already submited. wait please');
+            return false;
+        }
+        else {
+            tryNumber++;
+        }
+    };
+}); 
+
 
