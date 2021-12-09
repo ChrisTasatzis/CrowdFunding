8a91e13f232550d94d342b69a2eb558fc4a39f10
Geo Feid <fidakisgeorge@yahoo.com>
2021-12-10 00:21:50 +0200

    About us

--- CrowdFundingMVC/Controllers/HomeController.cs
+++ CrowdFundingMVC/Controllers/HomeController.cs
@@ -31,6 +31,11 @@ namespace CrowdFundingMVC.Controllers
             return View();
         }
 
+        public IActionResult Aboutus()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
